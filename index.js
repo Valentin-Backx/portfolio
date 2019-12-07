a818639f72fd703e137846b72c5146134b6cd98f
Valentin <v.backx@gmail.com>
2019-12-07 16:48:39 +0100

    env variable port management

--- index.js
+++ index.js
@@ -11,10 +11,27 @@ app.get('/', function (req, res) {
    res.sendfile('index')
 })
 
+var port = normalizePort(process.env.PORT || '8081');
 
-var server = app.listen(8081, function () {
+var server = app.listen(port, function () {
    var host = server.address().address
    var port = server.address().port
    
    console.log("Example app listening at http://%s:%s", host, port)
 })
+function normalizePort(val) {
+   var port = parseInt(val, 10);
+ 
+   if (isNaN(port)) {
+     // named pipe
+     return val;
+   }
+ 
+   if (port >= 0) {
+     // port number
+     return port;
+   }
+ 
+   return false;
+ }
+ 
